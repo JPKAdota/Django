61a1f6331f4c86093c039269d9300a89b6bc7dd0
JPKAdota <joaokadota@gmail.com>
2023-09-18 23:06:19 -0300

    Projeto: início

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def inicio(request):
-    return render(request, 'inicio.html')
+    resposta =  render(request, 'inicio.html')
+    return resposta
